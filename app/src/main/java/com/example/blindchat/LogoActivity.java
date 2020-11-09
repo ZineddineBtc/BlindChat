16863ca0af1f2e3b47dca04e65f26c80b4a9923f
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-11-09 14:22:00 +0100

    Logo/Slider implemented (entry)

--- app/src/main/java/com/example/blindchat/LogoActivity.java
+++ app/src/main/java/com/example/blindchat/LogoActivity.java
@@ -2,7 +2,15 @@ package com.example.blindchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class LogoActivity extends AppCompatActivity {
 
@@ -10,6 +18,15 @@ public class LogoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
-    }
 
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), SliderActivity.class));
+            }
+        }, 1000);
+    }
 }
