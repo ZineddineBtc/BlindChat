61a5f4abc56085b4c34242982dcd998ab180e28a
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-11-09 14:27:47 +0100

    Login implemented

---
+++ app/src/main/java/com/example/blindchat/User.java
@@ -0,0 +1,68 @@
+package com.example.blindchat;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+public class User {
+    private String id, username, name, bio;
+    private Bitmap photoBitmap;
+    private byte[] bytes;
+
+    public User(){}
+
+    public User(String id, String username, String name, String bio) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.bio = bio;
+    }
+
+    public byte[] getPhotoBytes() {
+        return bytes;
+    }
+
+    public void setPhotoBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public Bitmap getPhotoBitmap() {
+        return photoBitmap;
+    }
+
+    public void setPhotoBitmap(Bitmap photoBitmap) {
+        this.photoBitmap = photoBitmap;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
